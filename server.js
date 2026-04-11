e11c8cb3631733243e2557972de5de3e79ac772e
sousieevra-dev <sousieevra@gmail.com>
2026-04-11 23:43:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 app.listen(PORT, () => {
   console.log('Server started on port ' + PORT);
 });
